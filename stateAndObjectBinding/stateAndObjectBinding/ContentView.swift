8e2c85fe9d48cb8bf97bfc6b69e380aba1469348
thinkhatnag <thinkhatnag@gmail.com>
2025-11-19 14:36:52 +0530

    minute change

--- stateAndObjectBinding/stateAndObjectBinding/ContentView.swift
+++ stateAndObjectBinding/stateAndObjectBinding/ContentView.swift
@@ -4,48 +4,76 @@ import SwiftUI
 
 struct ContentView: View {
     
-       @State var isAuthenticated: Bool = false       //here if we did not used the @state and binding to make the object or data to be modified in the view.
+    //       @State var isAuthenticated: Bool = false       //here if we did not used the @state and binding to make the object or data to be modified in the view.
+    //
+    //        var body: some View {
+    //            Button(action: {
+    //                self.isAuthenticated.toggle()})
+    //            {
+    //                Text("Toggle authentication")
+    //            }
+    //        }
+    //    }
+    //
+    //            struct ContentView_Previews: PreviewProvider {
+    //                static var previews: some View {
+    //                    ContentView()
+    //                }
+    //            }
+    //
+    //
+    //
     
-        var body: some View {
+    
+    
+    
+    //     refrsh the state by button click
+    // @State var relodeCount = 0 // Here we want to mention that @State allows to update/modify the property in the struct bcz by default struct properties are immutable.
+    //    var body: some View {
+    //        Button(action: {
+    //            self.relodeCount += 1})
+    //        {
+    //           return Text("Welcome to IOS Learning Program \(relodeCount)")
+    //        }
+    //    }
+    
+    
+    //}
+    
+    
+    
+    
+    
+    //     refrsh the state by button click
+    @State var relodeCount = 0
+    @State var relode = 0// Here we want to mention that @State allows to update/modify the property in the struct bcz by default struct properties are immutable.
+    var body: some View {
+        
+        VStack{
             Button(action: {
-                self.isAuthenticated.toggle()})
+                self.relodeCount += 1})
             {
-                Text("Toggle authentication")
+                
+                return Text("Welcome to IOS Learning Program \(relodeCount)")
+            }
+            
+            Button(action: {
+                self.relode -= 2})
+            {
+                return Text("IOS Learning Program \(relodesB)")
             }
         }
     }
+}
     
-            struct ContentView_Previews: PreviewProvider {
-                static var previews: some View {
-                    ContentView()
-                }
-            }
+
+        
     
 
     
     
     
     
-//    
-////     refrsh the state by button click
-// @State var relodeCount = 0 // Here we want to mention that @State allows to update/modify the property in the struct bcz by default struct properties are immutable.
-//    var body: some View {
-//        Button(action: {
-//            self.relodeCount += 1})
-//        {
-//           return Text("Welcome to IOS Learning Program \(relodeCount)")
-//        }
-//    }
     
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    }
 
